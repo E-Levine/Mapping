e0945a10b29e08e7055740c2760c1e3864b1c21c
E-Levine <149699489+E-Levine@users.noreply.github.com>
2025-01-03 11:07:26 -0500

    HSI coding
    Project setup.
    Establishing code using SLE as example estuary.

---
+++ HSI/SLE/HSI_modeling_SLE.R
@@ -0,0 +1,27 @@
+###Working code for HSI creation.
+##Requires StateGrid picogrid layer, estuary area layer (+ any section area layers)
+#
+##General outline:
+#Load microgrid
+#Overlay various data types
+#Set scoring - make editable
+#Calculate HSI score
+#Output data, maps, etc.
+#
+#
+#.rs.restartR() #Restarts session (good if rerunning after working with other files)
+#graphics.off()  # turns off any plots from previous work session
+#rm(list=ls(all=TRUE)) # clears out environment 
+#
+#Load require packages (should install missing packages as necessary) - MAKE SURE PACMAN IS INSTALLED AND RUNNING!
+if (!require("pacman")) {install.packages("pacman")}
+pacman::p_load(plyr, tidyverse, #Df manipulation, basic summary
+               sf, raster, terra,
+               leaflet, tmap, 
+               install = TRUE) #Mapping and figures
+#
+#
+#Working parameters - to be set each time - need to be able to track data files used (i.e., WQ, Oyster layers, etc.)
+Site_Code <- c("SL") #two-letter site code
+State_Grid <- c("H4")
+Alt_State_Grid <- c(NA) #Two-letter StateGrid ID, enter NA if no secondary StateGrid needed
